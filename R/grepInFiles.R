806d0709badce38e58529c5f3d1070fb0b7c3189
stla <laurent_step@yahoo.fr>
2020-10-02 15:04:03 +0200

    first commit: grepInFiles

---
+++ R/grepInFiles.R
@@ -0,0 +1,52 @@
+#library(R.utils)
+
+# !! in Haskell, do a "cumulative" foldr when depth !!
+# => utilise filemanip (cf stlapblog)
+
+isPositiveInteger <- function(x){
+  is.numeric(x) && (length(x) == 1L) && (floor(x) == x)
+}
+
+getFiles <- function(ext, depth){
+  stopifnot(isPositiveInteger(depth))
+  wildcards <- Reduce(
+    file.path, x = rep("*", depth), init = paste0("*.", ext), 
+    right = TRUE, accumulate = TRUE
+  )
+  Sys.glob(wildcards)
+}
+
+grepInFiles <- function(
+  ext, pattern, depth = NULL, 
+  wholeword = FALSE, ignoreCase = FALSE, perl = FALSE,
+  directory = "."
+){
+  wd <- setwd(directory)
+  on.exit(setwd(wd))
+  opts <- c("--colour=always", "-n")
+  if(wholeword) opts <- c(opts, "-w")
+  if(ignoreCase) opts <- c(opts, "-i")
+  if(perl) opts <- c(opts, "-P")
+  if(is.null(depth)){
+    system2(
+      "grep",
+      args = c(
+        paste0("--include=\\*.", ext), opts, "-r", "-e", shQuote(pattern)
+      ), 
+      stdout = TRUE
+    )
+  }else{
+    files <- getFiles(ext, depth)
+    system2(
+      "grep",
+      args = c(shQuote(pattern), shQuote(files), opts),
+      stdout = TRUE
+    )
+  }
+}
+
+grepInFiles("Rmd", "document$")
+
+grepInFiles("Rmd", "document$", depth = 0)
+
+
